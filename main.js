19348cc736a3dcd40639cded8ad6d8cecc61a214
eugene li <hello@itseugene.li>
2017-09-01 19:00:24 -0400

    added a lot of ignored elements, fixed bugs, testing time restrictions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var builder = require("xmlbuilder");
 var request = require("request");
+var BigNumber = require("bignumber.js");
 var moment = require("moment");
 var AMCExcel = require("./AMCExcelProcessor");
 
@@ -11,7 +12,7 @@ if(args.length < 1)
     process.exit(1);
 }
 const AUTH_TOKEN = args[0];
-var writer = builder.streamWriter(fs.createWriteStream("test.xml"));
+var writer = builder.streamWriter(fs.createWriteStream("match14_test_export.xml"));
 
 var getSellingName = (dayPart) => {
     const SELLING_NAME = {
@@ -92,6 +93,31 @@ var getMatches = () => {
     });
 };
 
+var roundToTwoDecimals = (number) => parseInt(number).toFixed(2);
+
+var getRestrictions = (dayPart) => {
+    if(dayPart.value == "Daytime")  //Hours 6-14
+    {
+        return { start: 21600000, end: 50400000};
+    }
+    else if(dayPart.value == "Early Fringe") //Hours 14-17
+    {
+        return { start: 50400000, end: 61200000};
+    }
+    else if(dayPart.value == "Prime") //Hours 18-24
+    {
+        return { start: 64800000, end: 86400000};
+    }
+    else if(dayPart.value == "Late Night") //Hours 1-5
+    {
+        return { start: 3600000, end: 18000000};
+    }
+    else if(dayPart.value == "Weekend") //Hours 6-17 
+    {
+        return { start: 21600000, end: 61200000};
+    }
+};
+
 var processResponse = (resp) => {
     return new Promise((resolve, reject) => {
         var data = JSON.parse(resp);
@@ -107,7 +133,12 @@ var processResponse = (resp) => {
         quarter.start = quarter.start.format("MM/DD/YYYY");
         quarter.end = quarter.end.format("MM/DD/YYYY");
 
+        //For now just use daypart
+        var dayPart = data.match.buy.selectedAttrs.attributes.find((attr) => attr.type.name == "Daypart");
+        var restrictions = getRestrictions(dayPart);
+
         extras.quarter = quarter;
+        extras.restrictions = restrictions;
         
         data.extras = extras;
 
@@ -119,30 +150,53 @@ var buildXML = (data, agencyMap) => {
     var root = builder.create("WONetworkDeal");
 
     var header = root.ele("Header")
-        .ele("ChangeStatus", "Matched").up()
+        .ele("ChangeStatus", "New Deal").up()
         .ele("Property", "AMC").up() //Network?
-        .ele("DealStatusIncomplete", "Planning Costs").up()
-        .ele("DealStatusWorking", "Working").up()
-        .ele("TotalAmount", data.match.amount * data.match.matchedAparPrice).up()
+        .ele("DealStatusIncomplete", "Planning Costs").up() //blind copy from example
+        .ele("DealStatusWorking", "Working").up()           //blind copy from example
+        .ele("TotalAmount", roundToTwoDecimals(data.match.amount * data.match.matchedAparPrice)).up()
         .ele("TotalEQUnits", data.extras.adSize.value / 30).up() //Equiv 30s
-        .ele("Advertiser")
-        .ele("Name", data.campaign.advertiser).up()
+        .ele("TotalImps", 0).up();
+
+    var headerAdvertiser = header.ele("Advertiser")
+        .ele("Name", data.campaign.name).up()
+        .ele("Advertiser", "!PLACEHOLDER!").up()
         .ele("DealType", "MassEx").up()
         .ele("DealYear", getDealYear(data.match.buy.flightDate)).up()
+        .ele("Transaction", "Cash").up()    //blind copy from example
+        .ele("Marketplace", "Scatter").up() //blind copy from example
         .ele("RateCard", "AMC Mass Ex").up()
+        .ele("RatingStream", "C3").up()     //blind copy from example
+
+    var headerAdvertiserDemos = headerAdvertiser.ele("Demos")
+        .ele("Demo")
+        .ele("Name", "HH").up()             //blind copy from example (excel says "HH for now")
+        .ele("Primary", "Yes").up()         //blind copy from example
+        .ele("PostingExport", "Yes").up()   //blind copy from example
+        
+    headerAdvertiser.ele("Guaranteed", "No").up()
         .ele("PriorityCode", "MassEx").up()
         .ele("ExternalID", data.match.id).up() //"There is an ExternalID tag that can be used for [our matched order ID]"
         .ele("PriorityCode", "MassEx").up()
         .ele("Equivalized", "Yes").up()
-        .ele("Agency")
+
+    var agency = header.ele("Agency")
         .ele("Primary", `${data.match.buy.mp.name} (${agencyMap[data.match.buy.mp.name]})`);
+
+    var sales = header.ele("Sales")
+        .ele("AccountExecutives")
+        .ele("AccountExecutive")
+        .ele("Name", "Mass Exchange").up()  //Excel says "AE" in "AMC Master List"
+        .ele("Primary", "Yes").up()         //blind copy from example
+        .ele("Percent", 100);               //blind copy from example
 
     var flights = root.ele("Flights")
         .ele("Flight")
-        .ele("Start", data.campaign.flightStartDate).up()
-        .ele("End", data.campaign.flightEndDate).up()
-        .ele("Active", "Yes");
+        .ele("Start", data.extras.quarter.start).up()
+        .ele("End", data.extras.quarter.end);
+        //.ele("Active", "Yes");
 
+/*
     var advBrands = root.ele("AdvertiserBrands")
         .ele("AdvertiserBrand")
         .ele("Name", data.campaign.advertiser).up()
@@ -151,39 +205,55 @@ var buildXML = (data, agencyMap) => {
         .ele("Start", data.campaign.flightStartDate).up()
         .ele("End", data.campaign.flightEndDate).up()
         .ele("Active", "Yes");
-/*
+*/
+
     var guarantees = root.ele("Guarantees")
         .ele("Guarantee")
-        .ele("Start", data.campaign.flightStartDate).up()
-        .ele("End", data.campaign.flightEndDate).up()
+        .ele("Start", data.extras.quarter.start).up()
+        .ele("End", data.extras.quarter.end).up()
         .ele("Imps", "0").up()
-        .ele("TotalAmount", data.match.matchedAparPrice).up()
+        .ele("TotalAmount", roundToTwoDecimals(data.match.matchedAparPrice)).up()
         .ele("HHImps", "0");
-*/
+
     var targets = root.ele("Targets");
 
     var quarters = root.ele("Quarters")
         .ele("Quarter")
         .ele("Start", data.extras.quarter.start).up()
-        .ele("End", data.extras.quarter.end).up()
-        .ele("Line")
+        .ele("End", data.extras.quarter.end).up();
+
+    var line = quarters.ele("Line")
         .ele("SellingName", getSellingName(data.extras.dayPart.value)).up()
         .ele("InventoryDesc", getSellingName(data.extras.dayPart.value)).up()
         .ele("Start", data.campaign.flightStartDate).up()
         .ele("End", data.campaign.flightEndDate).up()
+        .ele("RateCard", "AMC Mass Ex").up() //Assuming "Ratecard" tab of AMC Master List
+        .ele("RatecardRate", 0).up()         //Excel says "not sure if 0"
         .ele("PriorityCode", "MassEx").up()
         .ele("SellingElement", "ME - MassEX").up()
         .ele("BreakCode", "National").up()
         .ele("Length", data.extras.adSize.value * 1000).up()
-        .ele("Rate", data.match.buy.aparPrice).up()
+        .ele("Rate", roundToTwoDecimals(data.match.buy.aparPrice)).up()
         .ele("Weekdays", getWeekString(data.match.buy.flightDate)).up()
-        .ele("UnitCount", data.match.amount).up()
-        .ele("Estimates").up()
-        .ele("Weeks")
+        .ele("UnitCount", data.match.amount).up();
+
+    var lineEstimates = line.ele("Estimates")
+        .ele("Estimate")
+        .ele("RatingStream", "C3").up()
+        .ele("Marketbreak", "General").up()
+        .ele("Demo", "HH").up()
+        .ele("Imps", 0);
+    
+console.log(data.extras.restrictions)
+    var lineWeeks = line.ele("Weeks")
         .ele("Week")
         .ele("Start", data.extras.firstMonday).up()
         //.ele("AdvertiserBrand", "WORK IN PROGRESS. CAMPAIGN'S UPCOMING 'BRAND' FIELD").up()
-        .ele("UnitCount", data.match.amount);
+        .ele("UnitCount", data.match.amount).up()
+        .ele("Weekdays", getWeekString(data.match.buy.flightDate)).up()
+        .ele("RestrictStart", data.extras.restrictions.start).up()
+        .ele("RestructEnd", data.extras.restrictions.end);
+
 
     return root; //root.end({ pretty: true});
 };
